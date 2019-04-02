972a29377b05bb79031319caaa1d07fc9acaaae5
muzrea <muzrea@hotmail.com>
2019-04-02 20:16:17 +0800

    create Order class

---
+++ src/main/model/Order.java
@@ -0,0 +1,25 @@
+package main.model;
+
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+import java.util.Map;
+import main.model.Dishes;
+public class Order {
+    private Map<Dishes,Double> orderInfo = new HashMap<Dishes,Double>();
+    public Order(Map<Dishes,Double> orderInfo){
+        this.orderInfo = orderInfo;
+    }
+    public void setOrderInfo(Map<Dishes,Double> orderInfo){
+        this.orderInfo = orderInfo;
+    }
+    public Map<Dishes,Double> getOrderInfo(){
+        return this.orderInfo;
+    }
+    public double getOrignalPrice(){
+        double originalPrice = 0.00;
+        for(Map.Entry<Dishes,Double> entry : this.orderInfo.entrySet()){
+            originalPrice += entry.getKey().getPrice()*entry.getValue();
+        }
+        return originalPrice;
+    }
+}
